9db544d9b6a8e8ba95550349af1d84e84b5b681f
Angelina <angelaaquin13@gmail.com>
2024-09-14 01:01:17 +0000

    push 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-
-
-	 
+import React, { useState , useEffect} from "react";	 
 
 const Home = () => {
 	
@@ -18,69 +15,91 @@ const Home = () => {
 		});
 		 
 		 setList (newList);
+		 
 	}
 
 	function enterFunc(event) {
 		let mykey = event.key;
 		if (mykey == "Enter") { 
-			setList([...list, [task]]);
+			addToDo(task);
+			getToDos();		
 		}
 	  }
 
 
-	fetch('https://playground.4geeks.com/todo/todos/Angelina_Renfroe', {
+	  const getToDos = () => {
+		fetch('https://playground.4geeks.com/todo/users/Angelina_Renfroe')
+		.then(response => {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			return response.json();
+		})
 
-		method: "PUT",
-	
-		body: JSON.stringify(list),
-	
-		headers: {
-	
-		  "Content-Type": "application/json"
-	
-		}
-	
-	  })
-	
-	  .then(resp => {
-	
-		  console.log(resp.ok); // Will be true if the response is successful
-	
-		  console.log(resp.status); // The status code=200 or code=400 etc.
-	
-		  console.log(resp.text()); // Will try to return the exact result as a string
-	
-		  return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
-	
-	  })
-	
-	  .then(data => {
-	
-		  // Here is where your code should start after the fetch finishes
-	
-		  console.log(data); // This will print on the console the exact object received from the server
-	
-	  })
-	
-	  .catch(error => {
-	
-		  // Error handling
-	
-		  console.error(error);
-	
-	  });
+		.then(responseAsJson => {
+			console.log(responseAsJson);
+			setList(responseAsJson.todos);				
+		})
+
+		.catch(error => {
+			console.log('Looks like there was a problem: \n', error);
+		});
+	  };
+
+	  const addToDo = (task) => {
+		fetch('https://playground.4geeks.com/todo/todos/Angelina_Renfroe', {
+
+			method: 'POST',
+			body:JSON.stringify({
+					'label': task,
+					'is_done': false
+				  }), // data can be a 'string' or an {object} which comes from somewhere further above in our application		  
+			headers: {		  
+			  'Content-Type': 'application/json'		  
+			}		  
+		  })
+		  
+			.then(res => {		  
+			  if (!res.ok) throw Error(res.statusText);		  
+			  return res.json();		  
+			})		  
+			.then(response => console.log('Success:', response))		  
+			.catch(error => console.error(error));
+	  };
+	  
+	const deleteToDo = () => {
+		fetch('https://playground.4geeks.com/todo/todos/Angelina_Renfroe', {
+        method: 'DELETE',
+		});
+		if (response.ok) {
+			const data =  response.json();
+			return data;
+
+		} else {
+			console.log('error: ', response.status, response.statusText);
+			/* Handle the error returned by the HTTP request */
+			return {error: {status: response.status, statusText: response.statusText}};
+		};
+
+	}
+
+	  useEffect(() => {
+		getToDos();	
+	}, []); //end of useEffect for on load
 
 	return (
-	<body className="mt-5">
+	<div className="mt-5">
 		<h2 className="text-center ">My Todos</h2>
 		<div className="inputLine text-center mb-2">
-			<input className="bg-light border border-light" id="taskInput" type="text" onChange= {(e)=> setTask(e.target.value)}  onKeyDown={(Event)=> enterFunc(Event)} placeholder="What do you need to do?"/>
+			<input className="bg-light border border-light" id="taskInput" type="text" 
+			onChange= {(e)=> setTask(e.target.value)}  
+			onKeyDown={(Event)=> enterFunc(Event)} placeholder="What do you need to do?"/>
 			
 		</div>
 		<div className="text-center">
 			<ul className="text-center p-0" >
 				{list.map((entry,index) =>
-					{return <li key={index} className="bg-light border border-secondary"> {entry}
+					{return <li key={index} className="bg-light border border-secondary"> {entry.label}
 					<button onClick= {()=> removeTask(index)} className="ms-3 btn text-center badge rounded-pill bg-secondary xBtn">X</button>
 					</li>}
 					)
@@ -89,7 +108,7 @@ const Home = () => {
 			</ul>
 		</div>
 		
-		</body>
+	</div>
 	);
 };
 

0e1ce4ea97758fd524806d688968849e9c94a81b
Angelina <angelaaquin13@gmail.com>
2024-09-27 21:23:24 +0000

    completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,33 +5,13 @@ const Home = () => {
 	const [task, setTask] = useState("");
 	const [list, setList] = useState([]);
 
-	function removeTask(index) {
-		var newList = [];
-		list.forEach(e => {
-			if(e !== list[index])
-			{newList =([...newList, e]);
-				console.log(newList);
-			}
-		});
-		 
-		 setList (newList);
-		 
-	}
-
-	function enterFunc(event) {
-		let mykey = event.key;
-		if (mykey == "Enter") { 
-			addToDo(task);
-			getToDos();		
-		}
-	  }
-
-
+	
 	  const getToDos = () => {
 		fetch('https://playground.4geeks.com/todo/users/Angelina_Renfroe')
 		.then(response => {
 			if (!response.ok) {
 				throw Error(response.statusText);
+				//if no profile create it first
 			}
 			return response.json();
 		})
@@ -46,7 +26,32 @@ const Home = () => {
 		});
 	  };
 
-	  const addToDo = (task) => {
+		const update =() =>
+		{
+			fetch('https://playground.4geeks.com/todo/users/Angelina_Renfroe', {
+		method: "PUT",
+		body: JSON.stringify(todos),
+		headers: {
+			"Content-Type": "application/json"
+		}
+		})
+		.then(resp => {
+			console.log(resp.ok); // Will be true if the response is successful
+			console.log(resp.status); // The status code=200 or code=400 etc.
+			console.log(resp.text()); // Will try to return the exact result as a string
+			return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+		})
+		.then(data => {
+			// Here is where your code should start after the fetch finishes
+			console.log(data); // This will print on the console the exact object received from the server
+		})
+		.catch(error => {
+			// Error handling
+			console.error(error);
+		});
+	}
+
+	  const addToDo = () => {
 		fetch('https://playground.4geeks.com/todo/todos/Angelina_Renfroe', {
 
 			method: 'POST',
@@ -63,25 +68,49 @@ const Home = () => {
 			  if (!res.ok) throw Error(res.statusText);		  
 			  return res.json();		  
 			})		  
-			.then(response => console.log('Success:', response))		  
+			.then(res => {
+				console.log('Success:', res)	
+				getToDos();
+				
+			
+			})	  
 			.catch(error => console.error(error));
 	  };
 	  
-	const deleteToDo = () => {
-		fetch('https://playground.4geeks.com/todo/todos/Angelina_Renfroe', {
-        method: 'DELETE',
-		});
-		if (response.ok) {
-			const data =  response.json();
-			return data;
-
-		} else {
-			console.log('error: ', response.status, response.statusText);
-			/* Handle the error returned by the HTTP request */
-			return {error: {status: response.status, statusText: response.statusText}};
-		};
-
+	const deleteToDo = (ind) => {
+		var id = ind.id;
+		
+		fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+        method: "DELETE",
+		headers:{
+			"Content-Type" : "application/json"
+		},
+		})
+		
+		.then((res) => {		  
+			if (!res.ok) {throw Error(res.statusText);}		  
+			getToDos();
+			return 
+				res.json();	
+					  
+		  })		  
+		  .then(res => {
+			  console.log('Successssss:', res);0	
+			  
+			  
+		  
+		  })	  
+		  .catch(error => console.error(error));
 	}
+		
+
+
+			function enterFunc(event) {
+				let mykey = event.key;
+				if (mykey == "Enter") { 
+					addToDo();
+				}
+			}
 
 	  useEffect(() => {
 		getToDos();	
@@ -92,19 +121,26 @@ const Home = () => {
 		<h2 className="text-center ">My Todos</h2>
 		<div className="inputLine text-center mb-2">
 			<input className="bg-light border border-light" id="taskInput" type="text" 
-			onChange= {(e)=> setTask(e.target.value)}  
-			onKeyDown={(Event)=> enterFunc(Event)} placeholder="What do you need to do?"/>
+			onChange= {(e)=> setTask(e.target.value)}  onKeyDown={(Event)=> enterFunc(Event)} placeholder="What do you need to do?"/>
 			
 		</div>
 		<div className="text-center">
 			<ul className="text-center p-0" >
 				{list.map((entry,index) =>
 					{return <li key={index} className="bg-light border border-secondary"> {entry.label}
-					<button onClick= {()=> removeTask(index)} className="ms-3 btn text-center badge rounded-pill bg-secondary xBtn">X</button>
+					<button onClick= {()=> deleteToDo(entry)} className="ms-3 btn text-center badge rounded-pill bg-secondary xBtn">X</button>
 					</li>}
 					)
 				}
-				<li>{list.length} items left</li>
+				<span>
+					<li>{list.length} items left</li>
+					<button onClick= {()=> 
+						{list.map((entry,index) =>
+							 deleteToDo(entry)
+							)
+						}
+					} className="ms-3 btn text-center badge rounded-pill bg-primary ">Delete all tasks</button>
+				</span>
 			</ul>
 		</div>
 		
